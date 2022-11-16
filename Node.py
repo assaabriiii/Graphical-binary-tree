28f0da7b33bc73031d2e7631b6e0fa87af9bae4f
amirsbry <amirsbry1942@gmail.com>
2022-11-16 11:39:04 +0330

    XD

--- Node.py
+++ Node.py
@@ -1,35 +1,93 @@
+import collections
+
+
 class Node : 
     def __init__(self , data )  :
         self.left = None  
         self.right = None  
         self.data = data 
     
-    def insert(self, data) : 
-        if data < self.data : 
-            if self.left : 
-                self.left.insert(data)
-            else : 
-                self.left = Node(data)
-                return
-        
-        else : 
-            if self.right : 
-                self.right.insert(data)
-            else : 
-                self.right = Node(data)
-                return
+class BinaryTree : 
+    def __init__(self , root) : 
+        self.root = Node(root)
     
-    def PrintTree(self) : 
-        if self.left : 
-            self.left.PrintTree()
-        print(self.data)
-        if self.right : 
-            self.right.PrintTree()
+    def PrintTree(self , trav_type) :
+        if trav_type == "preorder"  : 
+            return self.preorder_tree(tree.root , "")
+        elif trav_type == "inorder" :
+            return self.inorder_tree(tree.root , "")
+        elif trav_type == "postorder" : 
+            return self.postorder_tree(tree.root , "")
+        elif trav_type == "levelorder" : 
+            return self.levelorder_tree(tree.root)
+        else : 
+            return False 
+    
+    def preorder_tree(self , start , trav) : 
+        ## ROOT > LEFT > RIGHT
+        if start : 
+            trav += (str(start.data) + "__" )
+            trav = self.preorder_tree(start.left , trav) 
+            trav = self.preorder_tree(start.right , trav)
+        return trav 
+        
+    def inorder_tree(self , start , trav) : 
+        ## LEFT > ROOT > RIGHT
+        if start : 
+            trav = self.inorder_tree(start.left , trav) 
+            trav += (str(start.data) + "__")
+            trav = self.inorder_tree(start.right , trav)
+        return trav 
+    
+    def postorder_tree(self , start , trav)  :
+        ## LEFT > RIGHT > ROOT 
+        if start : 
+            trav = self.postorder_tree(start.left , trav) 
+            trav = self.postorder_tree(start.right , trav)
+            trav += (str(start.data) + "__")
+        return trav 
+    def levelorder_tree(self , start) :
+        ans = []
+        if start is None : 
+            return ans 
+        
+        queue = collections.deque()
+        queue.append(start)
+        
+        while queue :
+            currSize = len(queue)
+            currList = []
+            
+            while currSize > 0 :
+                currNode = queue.popleft()
+                currList.append(currNode.data)
+                currSize -= 1
+                
+                if currNode.left is not None : 
+                    queue.append(currNode.left)
+                    
+                if currNode.right is not None : 
+                    queue.append(currNode.right) 
+            ans.append(currList) 
+        return ans
+        
+            
+        """_summary_
+       2
+      /  \
+     3    4
+    / \
+   5   6
+   [2]
+   [3.4]
+   [5,6]
+        """
+    
 
-l = Node(2) 
-l.insert(5)         
-l.insert(1)         
-l.insert(7)         
-l.insert(9)         
-l.insert(10)
-l.PrintTree()         
+tree = BinaryTree(2)   
+tree.root.left = Node(3)
+tree.root.right = Node(4)
+tree.root.left.left = Node(5)
+tree.root.left.right = Node(6)
+
+print(tree.PrintTree("levelorder"))
