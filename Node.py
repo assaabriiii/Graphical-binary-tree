34c410df0f4b1e25c216bbd2f2288b834dd249f9
amirsbry <amirsbry1942@gmail.com>
2022-11-16 16:54:07 +0330

    XD

--- Node.py
+++ Node.py
@@ -1,5 +1,6 @@
 import collections
 
+NodesBeenMade = 0  
 
 class Node : 
     def __init__(self , data )  :
@@ -26,7 +27,9 @@ class BinaryTree :
     def preorder_tree(self , start , trav) : 
         ## ROOT > LEFT > RIGHT
         if start : 
-            trav += (str(start.data) + "__" )
+            global NodesBeenMade 
+            NodesBeenMade +=1 
+            trav += (str(start.data) + "," )
             trav = self.preorder_tree(start.left , trav) 
             trav = self.preorder_tree(start.right , trav)
         return trav 
@@ -34,17 +37,21 @@ class BinaryTree :
     def inorder_tree(self , start , trav) : 
         ## LEFT > ROOT > RIGHT
         if start : 
+            global NodesBeenMade 
+            NodesBeenMade +=1 
             trav = self.inorder_tree(start.left , trav) 
-            trav += (str(start.data) + "__")
+            trav += (str(start.data) + ",")
             trav = self.inorder_tree(start.right , trav)
         return trav 
     
     def postorder_tree(self , start , trav)  :
         ## LEFT > RIGHT > ROOT 
         if start : 
+            global NodesBeenMade 
+            NodesBeenMade +=1 
             trav = self.postorder_tree(start.left , trav) 
             trav = self.postorder_tree(start.right , trav)
-            trav += (str(start.data) + "__")
+            trav += (str(start.data) + ",")
         return trav 
     def levelorder_tree(self , start) :
         ans = []
@@ -70,7 +77,32 @@ class BinaryTree :
                     queue.append(currNode.right) 
             ans.append(currList) 
         return ans
+    
+    def floors_number(self) : 
+        result = tree.PrintTree("levelorder")
+        return len(result)
+    
+    def delete_tree(self) : 
+        tree.root = None 
         
+    def MAX_MIN(self) : 
+        result = tree.PrintTree("preorder")
+        result = result.split(",")
+        result.pop()
+        result = [int(x) for x in result ]
+        return max(result),min(result)
+    
+    def find(self , atribute) : 
+        result = tree.PrintTree("levelorder")
+        x = 0 
+        for i in result :
+            x +=1  
+            for j in i : 
+                if j == atribute : 
+                    return x
+        
+        
+    
             
         """_summary_
        2
@@ -82,7 +114,19 @@ class BinaryTree :
    [3.4]
    [5,6]
         """
-    
+# made a binary tree -> 1 
+# node class -> 1 
+# insert method -> 1
+# NodesBeenMade for number of nodes -> 1
+# number of floors -> 1
+# how to define number of leaves -> 0
+# made the 4 method and its working  -> 1 
+# delete the tree -> 1
+# MAX and MIN in our tree -> 1 
+# full tree -> 0 
+# compare of two tree -> 0 
+
+
 
 tree = BinaryTree(2)   
 tree.root.left = Node(3)
@@ -90,4 +134,11 @@ tree.root.right = Node(4)
 tree.root.left.left = Node(5)
 tree.root.left.right = Node(6)
 
+
+
+
 print(tree.PrintTree("levelorder"))
+result = tree.MAX_MIN()
+print("MAX and MIN is equal to : " , result)
+x = tree.find(5)
+print("Found the desired element at the : [" , x , "] floor ")
